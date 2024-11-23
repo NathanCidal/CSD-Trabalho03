7d2d8ccc2ef0bb698b91f433af03a2248eab1db6
NathanCidal <143615223+NathanCidal@users.noreply.github.com>
2024-11-22 21:41:28 -0300

    VGA_Sprite is default now

--- hf-riscv-software-vga/app/vga/vga_sprite.c
+++ hf-riscv-software-vga/app/vga/vga_sprite.c
@@ -24,61 +24,6 @@ char monster1b[8][11] = {
 	{0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0}
 };
 
-char earthprotector[8][11] = {
-	{0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0},
-	{0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0},
-	{0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
-};
-
-char barrier0bl[8][11] = {
-	{0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2},
-	{0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2},
-	{0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}	
-};
-
-char barrier0al[8][11] = {
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0},
-	{2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0},
-	{2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0},
-	{2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0}	
-};
-
-char barrier0ar[8][11] = {
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2},
-	{0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2},
-	{0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2},
-	{0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2}	
-};
-char barrier0br[8][11] = {
-	{2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0},
-	{2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}	
-};
-
-
 void draw_sprite(unsigned int x, unsigned int y, char *sprite,
 	unsigned int sizex, unsigned int sizey, int color)
 {
@@ -168,14 +113,6 @@ void init_display()
 	display_background(BLACK);
 }
 
-enum {
-	KEY_CENTER	= 0x01,		//MASK8
-	KEY_UP		= 0x02,		//MASK9
-	KEY_LEFT	= 0x04,		//MASK10
-	KEY_RIGHT	= 0x08,		//MASK11
-	KEY_DOWN	= 0x10		//MASK12
-};
-
 void init_input()
 {
 	/* configure GPIOB pins 8 .. 12 as inputs */
@@ -184,92 +121,28 @@ void init_input()
 
 int get_input()
 {
-	int keys = 0;
-	if (GPIOB->IN & MASK_P8)	keys |= KEY_CENTER;
-	if (GPIOB->IN & MASK_P9)	keys |= KEY_UP;
-	if (GPIOB->IN & MASK_P10)	keys |= KEY_LEFT;
-	if (GPIOB->IN & MASK_P11)	keys |= KEY_RIGHT;
-	if (GPIOB->IN & MASK_P12)	keys |= KEY_DOWN;
-		
-	return keys;
+	//No Code
 }
 
 /* main game loop */
 int main(void)
 {
-	struct object_s enemy1, enemy2, earthprotector_obj;
-
-
-	//barrier0al - Left Baixo
-	//barrior0ar - Right Baixo
-	//barrior0bl - Left Cima
-	//barrior0br - Right Cima
-	struct object_s barrier_l1, barrier_l2, barrier_r1, barrier_r2;
+	struct object_s enemy1, enemy2;
 
 	init_display();
 	init_input();
 
 	init_object(&enemy1, monster1a[0], monster1b[0], 0, 11, 8, 30, 35, 1, 1, 3, 3);
-	init_object(&earthprotector_obj, earthprotector[0], 0, 0, 11, 8, 150, 206, 1, 1, 3, 3);
+	init_object(&enemy2, monster1a[0], monster1b[0], 0, 11, 8, 10, 142, 3, 3, 1, 1);
 
-	init_object(&barrier_l1, barrier0al[0], 0, 0, 11, 8, 150, 196, 0, 0, 1, 1);
-	init_object(&barrier_r1, barrier0ar[0], 0, 0, 11, 8, 161, 196, 0, 0, 1, 1);
-	init_object(&barrier_l2, barrier0bl[0], 0, 0, 11, 8, 150, 188, 0, 0, 1, 1);
-	init_object(&barrier_r2, barrier0br[0], 0, 0, 11, 8, 161, 188, 0, 0, 1, 1);
-
-	int input_var;
-	int speedXPlayer = 0;
-	int speedYPlayer = 0;
 
 	while (1) {
 		move_object(&enemy1);
 		move_object(&enemy2);
 
-		move_object(&barrier_l1);
-		move_object(&barrier_r1);
-		move_object(&barrier_l2);
-		move_object(&barrier_r2);
-
-		input_var = get_input();
-		
-
-		//KEY_CENTER= 0x01,		//MASK8
-		//KEY_UP	= 0x02,		//MASK9
-		//KEY_LEFT	= 0x04,		//MASK10
-		//KEY_RIGHT	= 0x08,		//MASK11
-		//KEY_DOWN	= 0x10		//MASK12
-
-		switch (input_var){
-		 case KEY_LEFT:
-		 	speedXPlayer = -1;
-		 	//speedYPlayer = 0;
-		 	break;
-		 case KEY_RIGHT:
-		 	speedXPlayer = 1;
-		 	//speedYPlayer = 0;
-		 	break;
-		 case KEY_UP:
-		 	//speedXPlayer = 0;
-		 	speedYPlayer = -1;
-		 	break;
-		 case KEY_DOWN:
-		 	//speedXPlayer = 0;
-		 	speedYPlayer = 1;
-		 	break;
-		 default:
-		 	speedXPlayer = 0;
-			speedYPlayer = 0;
-		 	break;
-		 }
-
-		earthprotector_obj.dx = speedXPlayer;
-		earthprotector_obj.dy = speedYPlayer;
-
-		move_object(&earthprotector_obj);
-		
 		// you can change the direction, speed, etc...
 		
-		delay_ms(40);
+		delay_ms(200);
 	}
 
 	return 0;
